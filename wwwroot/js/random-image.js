3c2f15e27234a10c28967ad7a9ec29e8549d1a0a
tudorg <tudorg@indsoft.ro>
2024-04-09 12:11:06 +0300

    Front end improvments

---
+++ wwwroot/js/random-image.js
@@ -0,0 +1,12 @@
+﻿
+    document.addEventListener("DOMContentLoaded", function () {
+    var hero = document.getElementById("hero");
+        var images = [
+            '../Assets/image0_0.jpg',
+            '../Assets/image1_0.jpg',
+            '../Assets/image2_0.jpg'
+        ] 
+    var randomIndex = Math.floor(Math.random() * images.length);
+    var randomImage = images[randomIndex];
+    hero.style.backgroundImage = "url('" + randomImage + "')";
+        });
